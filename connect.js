c13aceda5898310a05fc76c2418940c25f5ea1cc
Nicholas Cable <cab17001@byui.edu>
2021-02-04 10:20:18 -0700

    some testing done

--- connect.js
+++ connect.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient;
 // var password = prompt("Please enter the password: ");
 // var database = prompt("Please enter the db name: ");
 
-const uri = `mongodb+srv://admin:${password}@cluster0.lwwzv.mongodb.net/${database}?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://admin:<password>@cluster0.lwwzv.mongodb.net/<db>?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useUnifiedTopology: true, useNewUrlParser: true });
 
 
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { MongoClient } = require("mongodb");
 // Replace the uri string with your MongoDB deployment's connection string.
-const uri = "mongodb+srv://admin:cLoud_management2021@cluster0.lwwzv.mongodb.net/test1?retryWrites=true&w=majority";
-const client = new MongoClient(uri);
+const uri = "mongodb+srv://admin:<password>@cluster0.lwwzv.mongodb.net/<db>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useUnifiedTopology: true, useNewUrlParser: true });
 async function run() {
     try {
         await client.connect();
-        const database = client.db('test1');
+        const database = client.db('<db>');
         const collection = database.collection('names');
         // Query for the names
         const query = { first_name: 'Bob' };
