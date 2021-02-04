054648af5f042c531a52f45c8814b992ab8dff1f
Nicholas Cable <cab17001@byui.edu>
2021-02-03 17:02:45 -0700

    added some changes

--- connect.js
+++ connect.js
@@ -1,17 +1,29 @@
-const { MongoClient } = require("mongodb");
- 
-// Replace the following with your Atlas connection string                                                                                                                                        
-const url = "mongodb+srv://admin:cLoud_management2021@cluster0.lwwzv.mongodb.net/test1?retryWrites=true&w=majority";
-const client = new MongoClient(url);
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:cLoud_management2021@cluster0.lwwzv.mongodb.net/sample_analytics?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+
+
 async function run() {
     try {
         await client.connect();
-        console.log("Connected correctly to server");
-    } catch (err) {
-        console.log(err.stack);
-    }
-    finally {
+        const collection = client.db("sample_analytics").collection("accounts");
+        // perform actions on the collection object
+        const query = { account_id: 557378 };
+        const results = await collection.findOne(query);
+        console.log(results);
+    } finally {
+        // Ensures that the client will close when you finish/error
         await client.close();
     }
 }
 run().catch(console.dir);
+
+// client.connect(err => {
+//   const collection = client.db("sample_analytics").collection("accounts");
+//   // perform actions on the collection object
+//   const query = {account_id: 557378};
+//   const results = await collection.findOne(query);
+//   console.log(results);
+//   client.close();
+// });
